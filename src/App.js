31dc2bda147cb1353c895e5c406be7c451f9daa7
Mus <138971833+Musxeto@users.noreply.github.com>
2024-02-20 18:58:21 +0500

    Im not good enough

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import './App.css';
-
+import "./App.css";
 function App() {
-  
-  return (
-    <div className="App">
-
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
