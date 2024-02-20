f97959af4fe7df3ef4074a154bf73a56f218bb74
Mus <138971833+Musxeto@users.noreply.github.com>
2024-02-20 17:36:09 +0500

    Finally Decided Bakcground

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import RandomQuote from './Components/RandomQuote';
-import NameQuote from './Components/NameQuote';
-import AnimeQuote from './Components/AnimeQuote';
+
 function App() {
   
   return (
     <div className="App">
-      <AnimeQuote />
-      <NameQuote />
-      <RandomQuote />
+
     </div>
   );
 }
