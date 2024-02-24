5021fd8a861ff6c21b3a91c270250c676b84f619
Mus <138971833+Musxeto@users.noreply.github.com>
2024-02-24 16:49:40 +0500

    Theme Switch BETA done

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className={`App ${currentTheme}`}>
       <div>
         <Navbar />
         <ThemesModal onThemeChange={handleThemeChange} />
