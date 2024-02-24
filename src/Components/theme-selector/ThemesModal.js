f88b4015ef46a0090313421fd93417394bf232a6
Mus <138971833+Musxeto@users.noreply.github.com>
2024-02-24 14:34:00 +0500

    CREATE THEME STATE AND REMOVED THE LINKS FROM IMGS IN THEMESMODAL

--- src/Components/theme-selector/ThemesModal.js
+++ src/Components/theme-selector/ThemesModal.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
 import "./ThemesModal.css";
 
 function ThemesModal() {
+  const [theme, setTheme] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
@@ -13,6 +14,12 @@ function ThemesModal() {
   const closeModal = () => {
     setIsModalOpen(false);
   };
+
+  useEffect(() => {
+    if (theme) {
+      closeModal();
+    }
+  }, [theme]);
 
   return (
     <div>
@@ -28,49 +35,80 @@ function ThemesModal() {
                 <div className="theme-selector">
                   <p className="label animated-text">S w i t c h T h e m e</p>
                   <div className="anime-logos">
-                    <a href="/naruto">
+                    <button
+                      className="button-gear"
+                      onClick={() => {
+                        setTheme("naruto");
+                        closeModal();
+                      }}
+                    >
                       <img
                         className="img-fluid"
                         src="/naruto-logo.png"
                         alt="Naruto Logo"
                       />
-                    </a>
-                    <a href="/jjk">
+                    </button>
+                    <button
+                      className="button-gear"
+                      onClick={() => {
+                        setTheme("jjk");
+                        closeModal();
+                      }}
+                    >
                       <img
                         className="img-fluid"
                         style={{ marginTop: "13px" }}
                         src="/jjk-logo.png"
                         alt="JJK Logo"
                       />
-                    </a>
-                    <a href="/bleach">
+                    </button>
+                    <button
+                      className="button-gear"
+                      onClick={() => {
+                        setTheme("bleach");
+                        closeModal();
+                      }}
+                    >
                       <img
                         className="img-fluid"
                         src="/bleach-logo.png"
                         alt="Bleach Logo"
                         style={{ marginTop: "-10px" }}
                       />
-                    </a>
-                    <a href="/demonslayer">
+                    </button>
+                    <button
+                      className="button-gear"
+                      onClick={() => {
+                        setTheme("demonslayer");
+                        closeModal();
+                      }}
+                    >
                       <img
                         style={{ marginTop: "-5px" }}
                         className="img-fluid"
                         src="/demonslayer-logo.png"
                         alt="Demon Slayer Logo"
                       />
-                    </a>
-                    <a href="/evangelion">
+                    </button>
+                    <button
+                      className="button-gear"
+                      onClick={() => {
+                        setTheme("evangelion");
+                        closeModal();
+                      }}
+                    >
                       <img
                         className="img-fluid"
                         src="/evangelion-logo.png"
                         alt="Evangelion Logo"
                       />
-                    </a>
+                    </button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          <h1>{theme}</h1>
           <button className="btn btn-secondary" onClick={closeModal}>
             Close
           </button>
